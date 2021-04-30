88b4a22343fa5263afbae4f63da53c7e7ca951ad
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-04-30 15:06:57 -0400

    Update tests

--- src/core/FileUpload/FileUpload.test.tsx
+++ src/core/FileUpload/FileUpload.test.tsx
@@ -3,11 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { FileUpload } from './FileUpload';
 
-it('should render in its most basic state', () => {
+it('should render content and children', () => {
   const mockContent = 'Mock drag content';
   const mockChildren = 'Mock children to wrap';
   const { container } = render(
@@ -26,4 +26,31 @@ it('should render in its most basic state', () => {
   const children = component.lastChild as HTMLElement;
   expect(children).toBeTruthy();
   expect(children.textContent).toEqual(mockChildren);
+});
+
+it('should handle drag and drop events correctly', () => {
+  const mockFn = jest.fn();
+  const { container } = render(
+    <FileUpload content={'Mock content'} onFileDropped={mockFn} />,
+  );
+
+  const component = container.querySelector('.iui-file-upload') as HTMLElement;
+  expect(component).toBeTruthy();
+
+  expect(container.querySelector('.iui-content')).toBeTruthy();
+
+  const file = new File(['mock-file'], 'test.txt', { type: 'text/plain' });
+  const mockDataTransfer = {
+    dataTransfer: { files: [file], items: [{ kind: 'file', type: file.type }] },
+  };
+
+  fireEvent.dragOver(component, mockDataTransfer);
+  expect(container.querySelector('.iui-file-upload.iui-drag')).toBeTruthy();
+
+  fireEvent.dragLeave(component, mockDataTransfer);
+  expect(container.querySelector('.iui-file-upload.iui-drag')).toBeFalsy();
+
+  fireEvent.dragOver(component, mockDataTransfer);
+  fireEvent.drop(component, mockDataTransfer);
+  expect(mockFn).toHaveBeenCalledWith([file]);
 });
