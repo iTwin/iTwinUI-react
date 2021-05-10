572e405820f94d2346e8ceb70b22330e66e446f5
Vilius <36186912+bentleyvk@users.noreply.github.com>
2021-05-10 16:46:41 +0300

    Comments

--- src/core/Table/filters/FilterButtonBar.tsx
+++ src/core/Table/filters/FilterButtonBar.tsx
@@ -8,11 +8,25 @@ import { useTheme } from '../../utils/hooks/useTheme';
 import { Button } from '../../Buttons';
 
 export type FilterButtonBarProps = {
+  /**
+   * Additional content that you want to be shown on the right of the buttons.
+   */
   children?: React.ReactNode;
+  /**
+   * Callback used for Filter button click.
+   */
   setFilter: () => void;
+  /**
+   * Callback used for Clear button click.
+   */
   clearFilter: () => void;
 };
 
+/**
+ * Button bar component that should be used in a filter. It already has Filter and Clear buttons.
+ * @example
+ * <FilterButtonBar setFilter={() => setFilter(text)} clearFilter={clearFilter} />
+ */
 export const FilterButtonBar = (props: FilterButtonBarProps) => {
   const { children, setFilter, clearFilter } = props;
 
--- src/core/Table/filters/types.ts
+++ src/core/Table/filters/types.ts
@@ -5,14 +5,35 @@
 import { FieldType, FilterType, HeaderGroup } from 'react-table';
 
 export interface TableFilterValue<T extends Record<string, unknown>> {
+  /**
+   * Id of the column.
+   */
   id: string;
+  /**
+   * Value of the filter.
+   */
   value: unknown;
+  /**
+   * Type of the column field.
+   */
   fieldType: FieldType;
+  /**
+   * Type of the filter.
+   */
   filterType: FilterType<T>;
 }
 
 export type TableFilterProps<T extends Record<string, unknown>> = {
+  /**
+   * Data of column on which filter is opened. It is provided by the table it self.
+   */
   column: HeaderGroup<T>;
+  /**
+   * Callback function to set the filter. It is provided by the table it self.
+   */
   setFilter: (filterValue: TableFilterValue<T> | undefined) => void;
+  /**
+   * Callback function to clear the filter. It is provided by the table it self.
+   */
   clearFilter: () => void;
 };
