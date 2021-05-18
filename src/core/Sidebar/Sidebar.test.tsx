014171c12c0241b3aaa6de18ae9ea148fa2116fb
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-05-18 17:45:42 -0400

    Add tests

--- src/core/Sidebar/Sidebar.test.tsx
+++ src/core/Sidebar/Sidebar.test.tsx
@@ -3,24 +3,25 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { Sidebar, SidebarProps } from './Sidebar';
 import { SidebarButton } from './SidebarButton';
 import SvgPlaceholder from '@itwin/itwinui-icons-react/cjs/icons/Placeholder';
+import SvgChevronRight from '@itwin/itwinui-icons-react/cjs/icons/ChevronRight';
 
 function renderComponent(props?: Partial<SidebarProps>) {
   return render(
     <Sidebar
       mainItems={[
         <SidebarButton startIcon={<SvgPlaceholder />} key={0}>
-          mockbutton1
+          mockbutton 0
         </SidebarButton>,
         <SidebarButton startIcon={<SvgPlaceholder />} key={1}>
-          mockbutton2
+          mockbutton 1
         </SidebarButton>,
         <SidebarButton startIcon={<SvgPlaceholder />} key={2}>
-          mockbutton3
+          mockbutton 2
         </SidebarButton>,
       ]}
       {...props}
@@ -34,12 +35,55 @@ it('should render in its most basic state', () => {
   expect(container.querySelector('.iui-sidenav-content')).toBeTruthy();
   expect(container.querySelector('.iui-expand')).toBeTruthy();
 
-  expect(
-    container.querySelectorAll('.iui-top .iui-sidenav-button'),
-  ).toHaveLength(3);
+  const {
+    container: { firstChild: placeholderIcon },
+  } = render(<SvgPlaceholder className='iui-icon' />);
+
+  const mainItems = container.querySelectorAll('.iui-top .iui-sidenav-button');
+  expect(mainItems).toHaveLength(3);
+  mainItems.forEach((item, index) => {
+    expect(item).toBeTruthy();
+    expect(item.querySelector('.iui-icon')).toEqual(placeholderIcon);
+    expect(item.querySelector('.iui-label')?.textContent).toBe(
+      `mockbutton ${index}`,
+    );
+  });
 });
 
-it('should render expand icon correctly', () => {
+it('should render secondary items', () => {
+  const { container } = renderComponent({
+    secondaryItems: [
+      <SidebarButton startIcon={<SvgPlaceholder />} key={0}>
+        mock secondary 0
+      </SidebarButton>,
+      <SidebarButton startIcon={<SvgPlaceholder />} key={1}>
+        mock secondary 1
+      </SidebarButton>,
+    ],
+  });
+  expect(container.querySelector('.iui-side-navigation')).toBeTruthy();
+  const mainItems = container.querySelectorAll('.iui-top .iui-sidenav-button');
+  expect(mainItems).toHaveLength(3);
+
+  const {
+    container: { firstChild: placeholderIcon },
+  } = render(<SvgPlaceholder className='iui-icon' />);
+
+  const secondaryItems = container.querySelectorAll(
+    '.iui-bottom .iui-sidenav-button',
+  );
+  expect(secondaryItems).toHaveLength(2);
+
+  secondaryItems.forEach((item, index) => {
+    expect(item).toBeTruthy();
+    expect(item.querySelector('.iui-icon')).toEqual(placeholderIcon);
+    expect(item.querySelector('.iui-label')?.textContent).toBe(
+      `mock secondary ${index}`,
+    );
+  });
+});
+
+it('should place expand button correctly in the DOM tree', () => {
   // top
   const { container: root1 } = renderComponent();
   const sidebar1 = root1.querySelector('.iui-side-navigation') as HTMLElement;
@@ -66,4 +110,100 @@ it('should render expand icon correctly', () => {
   expect(root3.querySelector('.iui-expand')).toBeFalsy();
 });
 
-// TODO: Write more tests here!
+it('should render expand button svg correctly', () => {
+  const { container } = renderComponent();
+  expect(container.querySelector('.iui-side-navigation')).toBeTruthy();
+
+  const {
+    container: { firstChild: expandIcon },
+  } = render(<SvgChevronRight className='iui-icon' aria-hidden />);
+  expect(container.querySelector('.iui-expand .iui-icon')).toEqual(expandIcon);
+});
+
+it('should handle clicking on expand button', () => {
+  const mockFn = jest.fn();
+  const { container } = renderComponent({ onExpanderClick: mockFn });
+  expect(
+    container.querySelector('.iui-side-navigation.iui-collapsed'),
+  ).toBeTruthy();
+
+  const expandButton = container.querySelector('.iui-expand') as HTMLElement;
+  expandButton.click();
+  expect(mockFn).toHaveBeenCalledTimes(1);
+  expect(
+    container.querySelector('.iui-side-navigation.iui-expanded'),
+  ).toBeTruthy();
+
+  expandButton.click();
+  expect(mockFn).toHaveBeenCalledTimes(2);
+  expect(
+    container.querySelector('.iui-side-navigation.iui-collapsed'),
+  ).toBeTruthy();
+});
+
+it('should work with controlled isExpanded prop', () => {
+  const { container: sidebar1 } = renderComponent({ isExpanded: true });
+  expect(
+    sidebar1.querySelector('.iui-side-navigation.iui-expanded'),
+  ).toBeTruthy();
+
+  const { container: sidebar2 } = renderComponent({ isExpanded: false });
+  expect(
+    sidebar2.querySelector('.iui-side-navigation.iui-collapsed'),
+  ).toBeTruthy();
+});
+
+it('should only add tooltips to items when collapsed', () => {
+  // collapsed
+  const { container, getByText, queryByText } = renderComponent();
+  expect(container.querySelector('.iui-side-navigation')).toBeTruthy();
+
+  const mainItems = container.querySelectorAll('.iui-top .iui-sidenav-button');
+  expect(mainItems).toHaveLength(3);
+  mainItems.forEach((item, index) => {
+    expect(
+      queryByText(`mockbutton ${index}`, { selector: '.iui-tooltip' }),
+    ).toBeFalsy();
+    fireEvent.mouseEnter(item);
+    getByText(`mockbutton ${index}`, { selector: '.iui-tooltip' });
+  });
+
+  // expanded
+  const expandButton = container.querySelector('.iui-expand') as HTMLElement;
+  expandButton.click();
+  expect(queryByText('mockbutton 0', { selector: '.iui-tooltip' })).toBeFalsy();
+});
+
+it('should render active and disabled sidebar buttons', () => {
+  const { container } = renderComponent({
+    mainItems: [
+      <SidebarButton startIcon={<SvgPlaceholder />} key={0} isActive>
+        mockbutton 0
+      </SidebarButton>,
+      <SidebarButton startIcon={<SvgPlaceholder />} key={1} disabled>
+        mockbutton 1
+      </SidebarButton>,
+    ],
+  });
+  expect(container.querySelector('.iui-side-navigation')).toBeTruthy();
+
+  const mainItems = container.querySelectorAll('.iui-top .iui-sidenav-button');
+  expect(mainItems).toHaveLength(2);
+
+  expect(mainItems[0].classList).toContain('iui-active');
+  expect((mainItems[1] as HTMLButtonElement).disabled).toBeTruthy();
+});
+
+it('should handle custom class and style', () => {
+  const { container } = renderComponent({
+    className: 'test-class',
+    style: { height: 200 },
+  });
+
+  const sidebar = container.querySelector(
+    '.iui-side-navigation.test-class',
+  ) as HTMLElement;
+
+  expect(sidebar).toBeTruthy();
+  expect(sidebar.style.height).toEqual('200px');
+});
