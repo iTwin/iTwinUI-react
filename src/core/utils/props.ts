0ce140f61d91bbd556799a78ed46f7d8a41b8a6d
Maxime <maxime.fontaine-bombardier@bentley.com>
2021-05-18 12:11:54 -0400

    Moving style prop in ClassNameProps

--- src/core/utils/props.ts
+++ src/core/utils/props.ts
@@ -7,13 +7,13 @@ export type ClassNameProps = {
    * Custom CSS class name.
    */
   className?: string;
-};
-
-export type CommonProps = {
   /**
    * Custom CSS style properties.
    */
   style?: React.CSSProperties;
+};
+
+export type CommonProps = {
   /**
    * HTML title attribute.
    */
