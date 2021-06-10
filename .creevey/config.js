7deb6cd2bac8767d79a566922e82f6d4a09b9bc8
veekeys <81580355+veekeys@users.noreply.github.com>
2021-06-10 13:35:02 +0300

    Error pages and es lint ignore

---
+++ .creevey/config.js
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+module.exports = {
+  // Pixelmatch options
+  diffOptions: { threshold: 0.1 },
+  // How many times test should be retried before to consider it as failed
+  maxRetries: 3,
+};
