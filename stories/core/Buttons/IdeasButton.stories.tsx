2369def896938d1077a98366198200529067f94f
veekeys <81580355+veekeys@users.noreply.github.com>
2021-06-10 11:57:30 +0300

    Some tests fixes

--- stories/core/Buttons/IdeasButton.stories.tsx
+++ stories/core/Buttons/IdeasButton.stories.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Story, Meta } from '@storybook/react';
 import { IdeasButton, IdeasButtonProps } from '../../../src/core';
+import { CreeveyStory } from 'creevey';
 
 export default {
   title: 'Buttons/IdeasButton',
@@ -18,14 +19,28 @@ export default {
   },
 } as Meta<IdeasButtonProps>;
 
-export const Ideas: Story<IdeasButtonProps> = (args) => {
+export const Ideas: Story<IdeasButtonProps> & CreeveyStory = (args) => {
   return <IdeasButton onClick={action('clicked')} {...args} />;
 };
 
-export const LocalizedIdeas: Story<IdeasButtonProps> = (args) => {
+Ideas.parameters = {
+  creevey: {
+    captureElement: null,
+  },
+};
+
+export const LocalizedIdeas: Story<IdeasButtonProps> & CreeveyStory = (
+  args,
+) => {
   return <IdeasButton onClick={action('clicked')} {...args} />;
 };
 
 LocalizedIdeas.args = {
   feedbackLabel: 'Localized feedback',
+};
+
+LocalizedIdeas.parameters = {
+  creevey: {
+    captureElement: null,
+  },
 };
