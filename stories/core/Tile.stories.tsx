02546bf3ffa85f2ad6aa04abf793c29f4b4d5233
Julius Rupšys <julius.rupsys@bentley.com>
2021-06-21 12:39:08 +0300

    Reduce flakiness by ignoring images in creevey tests

--- stories/core/Tile.stories.tsx
+++ stories/core/Tile.stories.tsx
@@ -21,6 +21,7 @@ import SvgInfo from '@itwin/itwinui-icons-react/cjs/icons/Info';
 import SvgStar from '@itwin/itwinui-icons-react/cjs/icons/Star';
 import SvgTag from '@itwin/itwinui-icons-react/cjs/icons/Tag';
 import { LargeWithImageAndStatusOnline } from './UserIcons.stories';
+import { CreeveyStoryParams } from 'creevey';
 
 export default {
   component: Tile,
@@ -89,6 +90,11 @@ Basic.argTypes = {
   rightIcon: { control: { disable: true } },
   badge: { control: { disable: true } },
 };
+Basic.parameters = {
+  creevey: {
+    ignoreElements: ['[style^="background-image"]'],
+  } as CreeveyStoryParams,
+};
 
 export const AllProps: Story<TileProps> = (props) => {
   const {
@@ -145,6 +151,11 @@ AllProps.args = {
     </Button>,
   ],
 };
+AllProps.parameters = {
+  creevey: {
+    ignoreElements: ['[style^="background-image"]'],
+  } as CreeveyStoryParams,
+};
 
 export const Condensed: Story<TileProps> = (props) => {
   const { name, thumbnail, moreOptions, ...rest } = props;
@@ -193,6 +204,11 @@ UserIcon.args = {
   description: 'User description',
   metadata: undefined,
   thumbnail: <LargeWithImageAndStatusOnline />,
+};
+UserIcon.parameters = {
+  creevey: {
+    ignoreElements: ['.iui-user-icon'],
+  } as CreeveyStoryParams,
 };
 
 export const Folder: Story<TileProps> = (props) => {
