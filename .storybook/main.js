6eadce2281681a2f2278efd6da96b9b082143fb4
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-06-24 11:12:36 -0400

    Add `@babel/plugin-proposal-class-properties`

--- .storybook/main.js
+++ .storybook/main.js
@@ -35,4 +35,12 @@ module.exports = {
     }
     return config;
   },
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: false }],
+      ['@babel/plugin-proposal-private-methods', { loose: false }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
+    ],
+  }),
 };
