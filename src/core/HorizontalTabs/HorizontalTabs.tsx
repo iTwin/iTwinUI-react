fbafe4ff371d56e40d02b5404dcca746f035a292
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-06-29 13:08:02 -0400

    Use hook to update tab stripe

--- src/core/HorizontalTabs/HorizontalTabs.tsx
+++ src/core/HorizontalTabs/HorizontalTabs.tsx
@@ -6,6 +6,7 @@ import cx from 'classnames';
 import React from 'react';
 import { useTheme } from '../utils/hooks/useTheme';
 import '@itwin/itwinui-css/css/tabs.css';
+import { useResizeObserver } from '../utils/hooks/useResizeObserver';
 
 export type HorizontalTabsProps = {
   /**
@@ -81,6 +82,8 @@ export const HorizontalTabs = (props: HorizontalTabsProps) => {
 
   const tablistRef = React.useRef<HTMLUListElement>(null);
 
+  const tablistSize = useResizeObserver(tablistRef);
+
   const [currentIndex, setCurrentIndex] = React.useState(getValidIndex());
   const [stripeStyle, setStripeStyle] = React.useState<React.CSSProperties>({});
 
@@ -88,14 +91,17 @@ export const HorizontalTabs = (props: HorizontalTabsProps) => {
     if (activeIndex != null && currentIndex !== activeIndex) {
       setCurrentIndex(getValidIndex());
     }
+  }, [activeIndex, currentIndex, getValidIndex]);
+
+  React.useLayoutEffect(() => {
     const activeTab = tablistRef.current?.children[currentIndex] as HTMLElement;
     if (type !== 'default') {
       setStripeStyle({
-        width: activeTab?.getBoundingClientRect().width,
+        width: activeTab?.getBoundingClientRect()?.width,
         left: activeTab?.offsetLeft,
       });
     }
-  }, [activeIndex, currentIndex, getValidIndex, type]);
+  }, [currentIndex, type, tablistSize]);
 
   const onTabClick = (index: number) => {
     if (onTabSelected) {
