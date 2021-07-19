f1b6184b7062fa80cd305b3a2b64bfb2d5825643
veekeys <81580355+veekeys@users.noreply.github.com>
2021-07-19 22:07:49 +0300

    cleanup tests setup

--- setupTests.js
+++ setupTests.js
@@ -4,14 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 window.HTMLElement.prototype.scrollIntoView = () => {};
 
-/* ---------------------------------------------------------------------------------------------
- * Below is hack to allow testing of PointerEvent as used in github package
- * https://github.com/pmndrs/use-gesture
- ---------------------------------------------------------------------------------------------*/
-/* eslint-disable no-console */
 if (window.PointerEvent) {
   console.error('ERROR: patching PointerEvent is no longer necessary');
 } else {
-  console.log('Setup: patching PointerEvent');
   window.PointerEvent = window.MouseEvent;
 }
