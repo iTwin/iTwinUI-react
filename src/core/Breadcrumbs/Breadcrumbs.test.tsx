3bb30012031bcba19803fe7a887ababb57715ad2
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-07-22 18:20:49 -0400

    Tests

--- src/core/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/core/Breadcrumbs/Breadcrumbs.test.tsx
@@ -5,16 +5,83 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { Breadcrumbs } from './Breadcrumbs';
+import { Breadcrumbs, BreadcrumbsProps } from './Breadcrumbs';
+import { BreadcrumbItem } from './BreadcrumbItem';
+import { SvgChevronRight } from '@itwin/itwinui-icons-react';
 
-it('should render in its most basic state', () => {
-  // TODO: Make sure all required props are passed in here
-  const { container } = render(<Breadcrumbs />);
-  expect(container.querySelector('div')).toBeTruthy();
+const renderComponent = (props?: Partial<BreadcrumbsProps>) => {
+  return render(
+    <Breadcrumbs
+      items={[...Array(3)].map((_, index) => (
+        <BreadcrumbItem key={index}>Item {index}</BreadcrumbItem>
+      ))}
+      {...props}
+    />,
+  );
+};
+
+const assertBaseElement = (
+  container: HTMLElement,
+  { itemsLength = 3, currentIndex = 2 } = {},
+) => {
+  expect(container.querySelector('.iui-breadcrumbs')).toBeTruthy();
+  expect(container.querySelector('.iui-breadcrumbs-list')).toBeTruthy();
+
+  // breadcrumb items
+  const breadcrumbs = container.querySelectorAll('.iui-breadcrumbs-item');
+  expect(breadcrumbs.length).toEqual(itemsLength);
+  breadcrumbs.forEach((breadcrumb, index) => {
+    expect(breadcrumb.textContent).toEqual(`Item ${index}`);
+
+    const item = breadcrumb.firstElementChild;
+    expect(item).toBeTruthy();
+
+    if (index === currentIndex) {
+      expect(breadcrumb.classList).toContain('iui-current');
+      expect(item?.getAttribute('aria-current')).toEqual('location');
+    } else {
+      expect(breadcrumb.classList).not.toContain('iui-current');
+      expect(item?.getAttribute('aria-current')).toBeFalsy();
+    }
+  });
+};
+
+it('should render all elements in default state', () => {
+  const { container } = renderComponent();
+  assertBaseElement(container);
+
+  const { container: chevron } = render(<SvgChevronRight />); // default separator
+
+  const separators = container.querySelectorAll('.iui-breadcrumbs-separator');
+  expect(separators.length).toEqual(2);
+  separators.forEach((separator) => {
+    expect(separator.getAttribute('aria-hidden')).toBeTruthy();
+    expect(separator.firstElementChild).toEqual(chevron.firstChild);
+  });
 });
 
-// TODO: Write tests here!
+it('should render custom separators', () => {
+  const { container } = render(
+    <Breadcrumbs
+      items={[...Array(3)].map((_, index) => (
+        <BreadcrumbItem key={index}>Item {index}</BreadcrumbItem>
+      ))}
+      separator='>'
+    />,
+  );
+  expect(container.querySelector('.iui-breadcrumbs')).toBeTruthy();
+  expect(container.querySelector('.iui-breadcrumbs-list')).toBeTruthy();
+  expect(container.querySelectorAll('.iui-breadcrumbs-item').length).toEqual(3);
 
-it('should be improved', () => {
-  expect(false).toBe(true);
+  const separators = container.querySelectorAll('.iui-breadcrumbs-separator');
+  expect(separators.length).toEqual(2);
+  separators.forEach((separator) => {
+    expect(separator.getAttribute('aria-hidden')).toBeTruthy();
+    expect(separator.textContent).toEqual('>');
+  });
+});
+
+it('should accept currentIndex prop', () => {
+  const { container } = renderComponent({ currentIndex: 1 });
+  assertBaseElement(container, { currentIndex: 1 });
 });
