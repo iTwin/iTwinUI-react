628fcd9e37ce3fd26c2de5dcae01af91adf97169
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-08-09 18:43:14 -0400

    Tests

--- src/core/Typography/Text/Text.test.tsx
+++ src/core/Typography/Text/Text.test.tsx
@@ -13,4 +13,40 @@ it('should render in the most basic state', () => {
   );
 });
 
-// TODO: add more tests
+it.each(['span', 'h3'] as const)('should render as %s element', (as) => {
+  const { container } = render(<Text as={as}>Some text</Text>);
+  expect(container.querySelector(`${as}.iui-text-block`)?.textContent).toEqual(
+    'Some text',
+  );
+});
+
+it.each(['headline', 'title', 'subheading', 'leading', 'small'] as const)(
+  'should render %s variant',
+  (variant) => {
+    const { container } = render(<Text variant={variant}>Some text</Text>);
+    expect(
+      container.querySelector(`div.iui-text-${variant}`)?.textContent,
+    ).toEqual('Some text');
+  },
+);
+
+it('should render as skeleton if isSkeleton is set', () => {
+  const { container } = render(<Text isSkeleton>Some text</Text>);
+  expect(
+    container.querySelector('div.iui-text-block.iui-skeleton')?.textContent,
+  ).toEqual('Some text');
+});
+
+it('should render as muted if isMuted is set', () => {
+  const { container } = render(<Text isMuted>Some text</Text>);
+  expect(
+    container.querySelector('div.iui-text-block.iui-text-muted')?.textContent,
+  ).toEqual('Some text');
+});
+
+it('should render with margin if hasMargin is set', () => {
+  const { container } = render(<Text hasMargin>Some text</Text>);
+  expect(
+    container.querySelector('div.iui-text-block.iui-text-spacing')?.textContent,
+  ).toEqual('Some text');
+});
