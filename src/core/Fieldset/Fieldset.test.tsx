c1feccfe07eb5e8d531a9b643751d31acfd154a8
Saally Campos <88331924+saaaaaally@users.noreply.github.com>
2021-08-10 10:49:01 -0500

    Cleaned up fieldset component and tests

--- src/core/Fieldset/Fieldset.test.tsx
+++ src/core/Fieldset/Fieldset.test.tsx
@@ -3,20 +3,32 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Fieldset } from './Fieldset';
 import { InputGroup } from '../InputGroup';
 import { ToggleSwitch } from '../ToggleSwitch';
+import '@testing-library/jest-dom/extend-expect';
+import { Select } from '../Select';
 
 it('should render in its most basic state', () => {
-  const { container } = render(<Fieldset legend='legend' />);
+  const { container } = render(
+    <Fieldset legend='legend'>
+      <InputGroup>
+        <ToggleSwitch />
+        <ToggleSwitch />
+      </InputGroup>
+    </Fieldset>,
+  );
 
   const fieldset = container.querySelector(
     '.iui-fieldset',
   ) as HTMLFieldSetElement;
   expect(fieldset).toBeTruthy();
   expect(fieldset.textContent).toBe('legend');
-  expect(fieldset.title).toBeFalsy();
+  expect(fieldset.disabled).toBe(false);
+
+  const inputs = container.querySelectorAll('input');
+  inputs.forEach((input) => expect(input).not.toBeDisabled());
 });
 
 it('should add className and style correctly', () => {
@@ -41,19 +53,53 @@ it('should render disabled group', () => {
   const { container } = render(
     <Fieldset legend='legend' disabled={true}>
       <InputGroup>
-        <ToggleSwitch disabled />
-        <ToggleSwitch disabled />
+        <ToggleSwitch />
+        <ToggleSwitch />
       </InputGroup>
     </Fieldset>,
   );
 
-  const fieldsetINPUT = container.querySelector(
+  const fieldset = container.querySelector(
     '.iui-fieldset',
   ) as HTMLFieldSetElement;
-  expect(fieldsetINPUT).toBeTruthy();
-  expect(fieldsetINPUT.disabled).toBe(true);
+  expect(fieldset).toBeTruthy();
+  expect(fieldset.disabled).toBe(true);
+
+  const inputContainers = container.querySelectorAll(
+    '.iui-input-container.iui-disabled',
+  );
+  expect(inputContainers.length).toBe(1);
 
   const inputs = container.querySelectorAll('input');
-  expect(inputs.length).toBe(2);
-  inputs.forEach((input) => expect(input.disabled).toBe(true));
+  inputs.forEach((input) => expect(input).toBeDisabled());
+});
+
+it('should render disabled select group', () => {
+  const mockedFn = jest.fn();
+  const { container } = render(
+    <Fieldset legend='legend' disabled={true}>
+      <Select
+        options={[
+          { value: 1, label: '1' },
+          { value: 2, label: '2' },
+        ]}
+      />
+    </Fieldset>,
+  );
+
+  const fieldset = container.querySelector(
+    '.iui-fieldset',
+  ) as HTMLFieldSetElement;
+  expect(fieldset).toBeTruthy();
+  expect(fieldset.disabled).toBe(true);
+
+  const selectButton = container.querySelector(
+    '.iui-select-button.iui-disabled',
+  ) as HTMLElement;
+  expect(selectButton).toBeTruthy();
+  selectButton.click();
+  expect(mockedFn).not.toHaveBeenCalled();
+  expect(selectButton.getAttribute('tabIndex')).toBeNull();
+  fireEvent.keyDown(selectButton, 'Spacebar');
+  expect(document.querySelector('.iui-menu')).toBeNull();
 });
--- src/core/Fieldset/Fieldset.tsx
+++ src/core/Fieldset/Fieldset.tsx
@@ -18,29 +18,12 @@ export type FieldsetProps = {
 /**
  * Basic fieldset component to group different components together
  * @example
- * <Fieldset legend='General Settings' {...args}>
- *  <LabeledSelect
- *    label='Resolution'
- *    options={[
- *      { value: 1, label: '1200 x 1000' },
- *      { value: 2, label: '1600 x 1200' },
- *      { value: 3, label: '2560 x 1600' },
- *    ]}
- *    displayStyle='inline'
- *    value={value}
- *    onChange={(value) => setValue(value)}
- *    placeholder='Select'
- *  />
- *  <InputGroup label='Color Theme' displayStyle='inline'>
- *    <Radio name='choice' value='option1' label={'Light'} />
- *    <Radio name='choice' value='option2' label={'Dark'} />
- *    <Radio name='choice' value='option3' label={'Match device'} />
- *  </InputGroup>
- *  <InputGroup>
- *    <ToggleSwitch label='Share usage statistics' />
- *    <ToggleSwitch label='Share crash logs' />
- *    <ToggleSwitch disabled label='Advanced settings' />
- *  </InputGroup>
+ * <Fieldset legend='Settings'>
+ *   <Input />
+ *   <InputGroup>
+ *     <ToggleSwitch />
+ *     <ToggleSwitch />
+ *   </InputGroup>
  * </Fieldset>
  */
 export const Fieldset = (props: FieldsetProps) => {
