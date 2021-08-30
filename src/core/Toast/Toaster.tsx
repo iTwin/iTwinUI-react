34c2b83d2c18d321da8eacd5ee5d8f8b6c142d55
Greta <greta.nausedaite@bentley.com>
2021-08-30 17:35:00 +0300

    Ooops

--- src/core/Toast/Toaster.tsx
+++ src/core/Toast/Toaster.tsx
@@ -15,7 +15,7 @@ export type ToasterSettings = {
    * Order of toasts.
    * Ascending places toasts from newest to oldest - most recent toasts are on top. Descending - from oldest to newest - most recent toasts are on bottom.
    * When placement is set and order not specified, toasts are ordered by placement. Top placement sets order 'descending', bottom placement sets order 'ascending'.
-   * @default 'ascending'
+   * @default 'descending'
    */
   order?: 'descending' | 'ascending';
   /**
