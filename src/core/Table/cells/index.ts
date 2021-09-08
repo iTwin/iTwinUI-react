2e5553b58ea4dc90d18aced6e4a852c55f014bc2
Vilius <36186912+bentleyvk@users.noreply.github.com>
2021-09-08 13:55:13 +0300

    copyright

--- src/core/Table/cells/index.ts
+++ src/core/Table/cells/index.ts
@@ -1,2 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 export { DefaultCell } from './DefaultCell';
 export { EditableCell } from './EditableCell';
