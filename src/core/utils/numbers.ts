08ba5f706a5e22b9e3eb49de679386865fa77335
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-09-30 08:53:38 -0400

    Remove nanoid

--- src/core/utils/numbers.ts
+++ src/core/utils/numbers.ts
@@ -2,9 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { nanoid } from 'nanoid';
-import { nanoid as nonsecureNanoid } from 'nanoid/non-secure';
-import { getWindow } from './dom';
 
 /**
  * Return input value bounded by specified range.
@@ -14,9 +11,13 @@ export const getBoundedValue = (val: number, min: number, max: number) => {
 };
 
 /**
- * Returns a random value using nanoid (which uses crypto API).
- * Contains IE fallback to return a uuid using Math.random.
+ * Returns a random value of a given length containing `A-Za-z0-9_-` symbols.
  */
-export const getRandomValue = (length?: number) => {
-  return getWindow()?.crypto ? nanoid(length) : nonsecureNanoid(length);
+export const getRandomValue = (length = 21) => {
+  const alphabet = `_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+  let id = '';
+  for (let i = 0; i < length; i++) {
+    id += alphabet[(Math.random() * 64) | 0];
+  }
+  return id;
 };
