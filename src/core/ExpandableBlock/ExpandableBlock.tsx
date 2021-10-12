13277294afced328682403234632f4b2898ca40b
Mayank <9084735+mayank99@users.noreply.github.com>
2021-10-12 17:18:38 -0400

    Add minWidth calculations

--- src/core/ExpandableBlock/ExpandableBlock.tsx
+++ src/core/ExpandableBlock/ExpandableBlock.tsx
@@ -2,138 +2,57 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import SvgChevronRight from '@itwin/itwinui-icons-react/cjs/icons/ChevronRight';
-import cx from 'classnames';
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 
-import {
-  CommonProps,
-  useTheme,
-  StatusIconMap,
-  WithCSSTransition,
-} from '../utils';
-import '@itwin/itwinui-css/css/expandable-block.css';
+export const WithCSSTransition = (
+  props: Partial<CSSTransitionProps> & {
+    children: JSX.Element;
+    dimension?: 'height' | 'width';
+  },
+) => {
+  const { in: visible, dimension = 'height', children, ...rest } = props;
 
-export type ExpandableBlockProps = {
-  /**
-   * The main text displayed on the block, regardless of state.
-   */
-  title: React.ReactNode;
-  /**
-   * Small note displayed above title, regardless of state.
-   */
-  caption?: React.ReactNode;
-  /**
-   * Status of the block.
-   * When set, a colored status icon is shown at the end of the main text.
-   */
-  status?: 'positive' | 'negative' | 'warning' | 'informational';
-  /**
-   * Svg icon displayed at the end of the main text.
-   * Will override status icon if specified.
-   */
-  endIcon?: JSX.Element;
-  /**
-   * Whether or not to show the block's content.
-   * @default false
-   */
-  isExpanded?: boolean;
-  /**
-   * Callback function for toggling an expansion state.
-   */
-  onToggle?: (isExpanding: boolean) => void;
-  /**
-   * Content in the expandable block.
-   */
-  children: React.ReactNode;
-} & Omit<CommonProps, 'title'>;
+  const expandedSize = React.useRef(0);
 
-/**
- * Container that allows content to be hidden behind a brief title and a caption.
- * @example
- * <ExpandableBlock title='Basic Block'>Content in block!</ExpandableBlock>
- * <ExpandableBlock title='Basic Block' caption='basic caption'>Content in block!</ExpandableBlock>
- * <ExpandableBlock title='Default Expanded Block' caption='basic caption' isExpanded={true}>Content in block!</ExpandableBlock>
- * <ExpandableBlock title='Positive status' status='positive'>Content</ExpandableBlock>
- * <ExpandableBlock title='Block with icon' endIcon={<SvgPlaceholder />}>Content</ExpandableBlock>
- */
-export const ExpandableBlock = (props: ExpandableBlockProps) => {
-  const {
-    caption,
-    children,
-    className,
-    title,
-    onToggle,
-    style,
-    isExpanded = false,
-    endIcon,
-    status,
-    ...rest
-  } = props;
-
-  useTheme();
-
-  const icon = endIcon ?? (status && StatusIconMap[status]());
-
-  const [expanded, setExpanded] = React.useState(isExpanded);
-  React.useEffect(() => {
-    setExpanded(isExpanded);
-  }, [isExpanded]);
-
-  const handleToggle = () => {
-    setExpanded(!expanded);
-    onToggle?.(!expanded);
-  };
-
-  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (
-      event.key === 'Enter' ||
-      event.key === ' ' ||
-      event.key === 'Spacebar'
-    ) {
-      handleToggle();
-    }
-  };
+  const dimensionCamelCase = dimension === 'height' ? 'Height' : 'Width';
 
   return (
-    <div
-      className={cx(
-        'iui-expandable-block',
-        { 'iui-with-caption': !!caption },
-        { 'iui-expanded': expanded },
-        className,
-      )}
-      style={style}
+    <CSSTransition
+      in={visible}
+      timeout={200}
+      unmountOnExit={true}
+      onEnter={(node) => {
+        node.style[`min${dimensionCamelCase}`] = 'initial';
+        node.style[dimension] = '0px';
+      }}
+      onEntering={(node) => {
+        node.style[dimension] = `${expandedSize.current}px`;
+      }}
+      onEntered={(node) => {
+        node.style[`min${dimensionCamelCase}`] = '';
+        node.style[dimension] = '';
+      }}
+      onExit={(node) => {
+        node.style[dimension] = `${expandedSize.current}px`;
+      }}
+      onExiting={(node) => {
+        node.style[`min${dimensionCamelCase}`] = 'initial';
+        node.style[dimension] = '0px';
+      }}
+      classNames='iui'
       {...rest}
     >
-      <div
-        aria-expanded={expanded}
-        className='iui-header'
-        tabIndex={0}
-        onClick={handleToggle}
-        onKeyDown={onKeyDown}
-      >
-        <SvgChevronRight className='iui-icon' aria-hidden />
-        <span className='iui-expandable-block-label'>
-          <div className='iui-title'>{title}</div>
-          {caption && <div className='iui-caption'>{caption}</div>}
-        </span>
-        {icon &&
-          React.cloneElement(icon, {
-            className: cx(
-              'iui-status-icon',
-              { [`iui-${status}`]: !!status },
-              icon.props.className,
-            ),
-          })}
-      </div>
-      <WithCSSTransition in={expanded}>
-        <div className='iui-expandable-content'>
-          <div>{children}</div>
-        </div>
-      </WithCSSTransition>
-    </div>
+      {React.cloneElement(children, {
+        ref: (el: HTMLElement) => {
+          if (el) {
+            expandedSize.current = el.getBoundingClientRect()[dimension];
+          }
+        },
+      })}
+    </CSSTransition>
   );
 };
 
-export default ExpandableBlock;
+export default WithCSSTransition;
