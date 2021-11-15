74f601602ab002f8675e698d01ed361ad642846d
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-11-15 13:46:26 -0500

    Fix CSS issues and add stories

--- src/core/index.ts
+++ src/core/index.ts
@@ -107,6 +107,9 @@ export type {
 export { Input } from './Input';
 export type { InputProps } from './Input';
 
+export { Label } from './Label';
+export type { LabelProps } from './Label';
+
 export { LabeledInput } from './LabeledInput';
 export type { LabeledInputProps } from './LabeledInput';
 
@@ -247,6 +250,3 @@ export type {
 
 export { getUserColor, useTheme, ColorValue } from './utils';
 export type { ThemeType } from './utils';
-
-export { Label } from './Label';
-export type { LabelProps } from './Label';
