0a9621d15ea19fab3515d0b4c84bb53e557c110c
Mayank Agarwal <9084735+mayank99@users.noreply.github.com>
2021-11-18 10:46:53 -0500

    Clean up tests

--- src/core/utils/hooks/useContainerWidth.test.tsx
+++ src/core/utils/hooks/useContainerWidth.test.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-// import React from 'react';
 import { useContainerWidth } from './useContainerWidth';
 import * as UseResizeObserver from './useResizeObserver';
 import { renderHook, act } from '@testing-library/react-hooks';
@@ -15,16 +14,20 @@ afterEach(() => {
   jest.restoreAllMocks();
 });
 
-it('should set initial size', () => {
-  const { result } = renderHook(() => useContainerWidth());
+const renderHookComponent = (watchResizes = true) => {
+  const hook = renderHook(() => useContainerWidth(watchResizes));
   const element = document.createElement('div');
   Object.defineProperties(element, {
     getBoundingClientRect: { value: () => ({ width: 100 }) },
   });
   act(() => {
-    result.current[0](element);
+    hook.result.current[0](element);
   });
+  return hook;
+};
 
+it('should set initial size', () => {
+  const { result } = renderHookComponent();
   expect(result.current[1]).toEqual(100);
 });
 
@@ -39,14 +42,7 @@ it('should update value when resized', () => {
         ({ disconnect: jest.fn() } as unknown) as ResizeObserver,
       ];
     });
-  const { result } = renderHook(() => useContainerWidth());
-  const element = document.createElement('div');
-  Object.defineProperties(element, {
-    getBoundingClientRect: { value: () => ({ width: 100 }) },
-  });
-  act(() => {
-    result.current[0](element);
-  });
+  const { result } = renderHookComponent();
 
   expect(result.current[1]).toEqual(100);
 
@@ -68,14 +64,7 @@ it('should not update value when resized if disabled', () => {
       triggerResize = onResize;
       return [jest.fn(), ({ disconnect } as unknown) as ResizeObserver];
     });
-  const { result } = renderHook(() => useContainerWidth(false));
-  const element = document.createElement('div');
-  Object.defineProperties(element, {
-    getBoundingClientRect: { value: () => ({ width: 100 }) },
-  });
-  act(() => {
-    result.current[0](element);
-  });
+  const { result } = renderHookComponent(false);
 
   expect(result.current[1]).toEqual(100);
 
