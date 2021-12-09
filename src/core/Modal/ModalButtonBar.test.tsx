20ce53808f76532e49dbfd4d1d92954d3b2f48da
Mayank <9084735+mayank99@users.noreply.github.com>
2021-12-09 12:54:21 -0500

    fix formatting

--- src/core/Modal/ModalButtonBar.test.tsx
+++ src/core/Modal/ModalButtonBar.test.tsx
@@ -2,31 +2,31 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import { Button } from "../Buttons";
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Button } from '../Buttons';
 
-import { ModalButtonBar } from "./ModalButtonBar";
+import { ModalButtonBar } from './ModalButtonBar';
 
-it("should render in its most basic state", () => {
+it('should render in its most basic state', () => {
   const { container } = render(
     <ModalButtonBar>
       <Button>TestBtn1</Button>
       <Button>TestBtn2</Button>
-    </ModalButtonBar>
+    </ModalButtonBar>,
   );
-  expect(container.querySelector(".iui-button-bar")).toBeTruthy();
-  screen.getByText("TestBtn1");
-  screen.getByText("TestBtn2");
+  expect(container.querySelector('.iui-button-bar')).toBeTruthy();
+  screen.getByText('TestBtn1');
+  screen.getByText('TestBtn2');
 });
 
-it("should propagate miscellaneous props", () => {
+it('should propagate miscellaneous props', () => {
   const { container } = render(
-    <ModalButtonBar className="test-class" id="test-id" style={{ gap: 8 }} />
+    <ModalButtonBar className='test-class' id='test-id' style={{ gap: 8 }} />,
   );
 
-  const buttonBar = container.querySelector(".iui-button-bar") as HTMLElement;
-  expect(buttonBar).toHaveClass("test-class");
-  expect(buttonBar).toHaveStyle("gap: 8px;");
-  expect(buttonBar.id).toEqual("test-id");
+  const buttonBar = container.querySelector('.iui-button-bar') as HTMLElement;
+  expect(buttonBar).toHaveClass('test-class');
+  expect(buttonBar).toHaveStyle('gap: 8px;');
+  expect(buttonBar.id).toEqual('test-id');
 });
