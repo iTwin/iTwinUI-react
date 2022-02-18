a44ca8270b338d87a9dbc343ae7b0aa6a24a047a
adamhock <78564221+adamhock@users.noreply.github.com>
2022-02-18 09:46:28 -0500

    Column Manager has functionality

--- src/core/Table/hooks/useColumnVisibility.tsx
+++ src/core/Table/hooks/useColumnVisibility.tsx
@@ -3,40 +3,20 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
-import { ColumnInstance, HeaderProps, Hooks, TableInstance } from 'react-table';
+import { ColumnInstance, HeaderProps, Hooks } from 'react-table';
 import { Checkbox } from '../..';
 import SvgColumnManager from '@itwin/itwinui-icons-react/cjs/icons/ColumnManager';
 import { DropdownButton } from '../../Buttons';
+import { isString } from 'creevey';
 
 export const COLUMN_MANAGER_ID = 'iui-table-column-manager';
 
 export const useColumnVisibility = <T extends Record<string, unknown>>(
   isManageable: boolean,
 ) => (hooks: Hooks<T>) => {
-  hooks.useInstance.push(useInstance.bind({}, isManageable));
-  // const menuItems = (close: () => void) => [
-  //   <MenuItem key={1}>Item #1</MenuItem>,
-  //   <MenuItem key={2}>Item #2</MenuItem>,
-  // ];
-
-  // const inputGroup = (close: () => void) => [
-  //   <InputGroup displayStyle='default' key={1}>
-  //     <Checkbox checked />
-  //     <Checkbox checked label='Football' />
-  //     <Checkbox checked label='Hockey' />
-  //   </InputGroup>,
-  // ];
-
-  // const headerCheckBoxes = (close: () => void) =>
-  //   hooks.allColumns.map((column) => (
-  //     <Checkbox checked label={column.name} key={1} />
-  //   ));
-
-  // const checkBoxes = (close: () => void) => [
-  //   <Checkbox checked key={1} />,
-  //   <Checkbox checked label='Football' key={2} />,
-  //   <Checkbox checked label='Hockey' key={3} />,
-  // ];
+  if (!isManageable) {
+    return;
+  }
 
   hooks.allColumns.push((columns: ColumnInstance<T>[]) => [
     ...columns,
@@ -49,19 +29,19 @@ export const useColumnVisibility = <T extends Record<string, unknown>>(
       maxWidth: 48,
       columnClassName: 'iui-slot',
       cellClassName: 'iui-slot',
-      Header: ({ columns }: HeaderProps<T>) => {
+      Header: ({ allColumns, column, headers }: HeaderProps<T>) => {
         const headerCheckBoxes = (close: () => void) =>
-          columns[0].columns
-            ? columns[0].columns.map((column) => (
-                <Checkbox checked label={column.Header} key={column.id} />
-              ))
-            : columns.map((column) => (
-                <Checkbox
-                  checked
-                  label={column.Header?.toString}
-                  key={column.id}
-                />
-              ));
+          allColumns.map((column) =>
+            isString(column.Header) ? (
+              <Checkbox
+                {...column.getToggleHiddenProps}
+                label={column.Header}
+                key={column.id}
+              />
+            ) : (
+              <></>
+            ),
+          );
         return (
           <DropdownButton menuItems={headerCheckBoxes} styleType='borderless'>
             <SvgColumnManager className='iui-button-icon' aria-hidden />
@@ -70,64 +50,4 @@ export const useColumnVisibility = <T extends Record<string, unknown>>(
       },
     },
   ]);
-};
-
-const useInstance = <T extends Record<string, unknown>>(
-  isManageable: boolean,
-  instance: TableInstance<T>,
-) => {
-  // const defaultGetToggleHiddenProps = (isManageable: boolean | undefined) => (
-  //   props: TableKeyedProps,
-  //   {
-  //     instance,
-  //     header,
-  //     nextHeader,
-  //   }: {
-  //     instance: TableInstance;
-  //     header: HeaderGroup;
-  //     nextHeader: HeaderGroup;
-  //   },
-  // ) => {
-  //   if (!isManageable) {
-  //     return props;
-  //   }
-  //   return [
-  //     props,
-  //     {
-  //       onChange: (e: React.ChangeEvent) => {
-  //         header.column.toggleHidden(!e.target);
-  //       },
-  //       style: {
-  //         cursor: 'pointer',
-  //       },
-  //       checked: column.isVisible,
-  //       title: 'Toggle Column Visible',
-  //     },
-  //   ]
-  // };
-  // {isManageable && (
-  //   <div>
-  //     <button
-  //       className='iui-button iui-borderless'
-  //       aria-label='Modify columns'
-  //       type='button'
-  //     >
-  //       <SvgColumnManager
-  //         className='iui-button-icon'
-  //         aria-hidden
-  //       />
-  //     </button>
-  //   </div>
-  // )}
-  // return (
-  //   <DropdownButton
-  //     type='button'
-  //     menuItems={menuItems}
-  //     styleType='borderless'
-  //   >
-  //     <IconButton styleType='borderless'>
-  //       <SvgColumnManager className='iui-button-icon' aria-hidden />
-  //     </IconButton>
-  //   </DropdownButton>
-  // );
 };
