191dce03d4ad25fc93ad8a95dba93f310ad1d09a
Vilius <vilius.kazlauskas@bentley.com>
2022-02-21 13:40:11 +0200

    Really basic story

--- stories/core/Tree.stories.tsx
+++ stories/core/Tree.stories.tsx
@@ -34,6 +34,95 @@ export const Basic: Story<TreeProps<unknown>> = () => {
     subItems: StoryData[];
   };
 
+  const [expandedNodes, setExpandedNodes] = React.useState<
+    Record<string, boolean>
+  >({
+    'Node-2': true,
+    'Node-2-1': true,
+    'Node-3': true,
+  });
+  const onNodeExpanded = React.useCallback(
+    (nodeId: string, isExpanded: boolean) => {
+      if (isExpanded) {
+        setExpandedNodes((oldExpanded) => ({ ...oldExpanded, [nodeId]: true }));
+        action(`Expanded node ${nodeId}`)();
+      } else {
+        setExpandedNodes((oldExpanded) => ({
+          ...oldExpanded,
+          [nodeId]: false,
+        }));
+        action(`Closed node ${nodeId}`)();
+      }
+    },
+    [],
+  );
+  const generateItem = React.useCallback(
+    (index: number, parentNode = '', depth = 0): StoryData => {
+      const keyValue = parentNode ? `${parentNode}-${index}` : `${index}`;
+      return {
+        id: `Node-${keyValue}`,
+        label: `Node ${keyValue}`,
+        sublabel: `Sublabel for Node ${keyValue}`,
+        subItems:
+          depth < 10
+            ? Array(Math.round(index % 5))
+                .fill(null)
+                .map((_, index) => generateItem(index, keyValue, depth + 1))
+            : [],
+      };
+    },
+    [],
+  );
+
+  const data = React.useMemo(
+    () =>
+      Array(50)
+        .fill(null)
+        .map((_, index) => generateItem(index)),
+    [generateItem],
+  );
+
+  const getNode = React.useCallback(
+    (node: StoryData): NodeData<StoryData> => {
+      return {
+        subNodes: node.subItems,
+        nodeId: node.id,
+        node: node,
+        isExpanded: expandedNodes[node.id],
+        hasSubNodes: node.subItems.length > 0,
+      };
+    },
+    [expandedNodes],
+  );
+
+  return (
+    <Tree<StoryData>
+      data={data}
+      getNode={getNode}
+      nodeRenderer={React.useCallback(
+        ({ node, ...rest }) => (
+          <TreeNode
+            label={node.label}
+            sublabel={node.sublabel}
+            onExpanded={onNodeExpanded}
+            icon={<SvgPlaceholder />}
+            {...rest}
+          />
+        ),
+        [onNodeExpanded],
+      )}
+    />
+  );
+};
+
+export const Full: Story<TreeProps<unknown>> = () => {
+  type StoryData = {
+    id: string;
+    label: string;
+    sublabel: string;
+    subItems: StoryData[];
+  };
+
   const [selectedNodes, setSelectedNodes] = React.useState<
     Record<string, boolean>
   >({
