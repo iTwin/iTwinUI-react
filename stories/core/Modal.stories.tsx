d9e6e5e9f21c483e4147ff1915997fd07b9463a1
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2022-03-21 11:02:47 -0500

    Added ModalContent, modeled after ModalButtonBar.

--- stories/core/Modal.stories.tsx
+++ stories/core/Modal.stories.tsx
@@ -11,6 +11,7 @@ import { Modal, ModalButtonBar } from '../../src/core/Modal';
 import { ModalProps } from '../../src/core/Modal/Modal';
 import { useEffect, useState } from '@storybook/addons';
 import { CreeveyMeta } from 'creevey';
+import ModalContent from '../../src/core/Modal/ModalContent';
 
 export default {
   title: 'Core/Modal',
@@ -90,15 +91,17 @@ export const Basic: Story<ModalProps> = ({
         onKeyDown={action('onKeyDown', { depth: 1 })}
         {...rest}
       >
-        <div className='iui-modal-content'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+        <ModalContent>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </ModalContent>
         <ModalButtonBar>
           <Button styleType='high-visibility' onClick={primaryButtonHandle}>
             Primary
