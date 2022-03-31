9486d3bcd40e811b242445e32e0b9e87bec3b296
adamhock <78564221+adamhock@users.noreply.github.com>
2022-03-30 22:13:24 -0400

    column manager refactor matches default column PR

---
+++ src/core/Table/columns/index.ts
@@ -0,0 +1,5 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+export { COLUMN_MANAGER_ID, ManagerColumn } from './managerColumn';
