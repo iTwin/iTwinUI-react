5f73759cf38790dc3f58b258921b1b1504047e84
Mayank <mayank99@users.noreply.github.com>
2022-04-01 14:30:00 -0400

    Move to monorepo setup

---
+++ apps/playground/src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Button } from '@itwin/itwinui-react';
+
+const App = () => {
+  return <Button>Hello world</Button>;
+};
+
+export default App;
---
+++ apps/playground/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import linaria from 'vite-plugin-linaria'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), linaria()],
+  server: {
+    port: 4000
+  }
+})
