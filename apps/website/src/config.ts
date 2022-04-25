c6daf3f48b7732065bff8eee824d473ff6038543
Mayank <mayank99@users.noreply.github.com>
2022-04-25 13:58:39 -0400

    Initial astro implementation

---
+++ apps/website/src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  title: 'iTwinUI',
+  description: 'An open source design system for building consistent interfaces',
+  defaultLanguage: 'en_US',
+};
+
+export const OPEN_GRAPH = {
+  image: {
+    src: '',
+    alt: 'itwinui logo',
+  },
+};
+
+export const SIDEBAR = [
+  { text: '', header: true },
+  { text: 'Section Header', header: true },
+  { text: 'Introduction', link: 'docs/introduction' },
+  { text: 'Page 2', link: 'docs/page-2' },
+  { text: 'Page 3', link: 'docs/page-3' },
+
+  { text: 'Another Section', header: true },
+  { text: 'Page 4', link: 'docs/page-4' },
+];
