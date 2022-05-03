bedc4fd45e231308e21db19ffbd8eaceeed1b274
Vilius <vilius.kazlauskas@bentley.com>
2022-05-03 12:42:51 +0300

    fix for slider

--- packages/iTwinUI-react/stories/core/Slider.stories.tsx
+++ packages/iTwinUI-react/stories/core/Slider.stories.tsx
@@ -21,6 +21,11 @@ export default {
     thumbMode: 'inhibit-crossing',
     trackDisplayMode: 'auto',
   },
+  parameters: {
+    creevey: {
+      delay: 1000,
+    },
+  },
 } as Meta<SliderProps>;
 
 export const Basic: Story<SliderProps> = (args) => {
