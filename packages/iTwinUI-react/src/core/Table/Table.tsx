58e5b68c2eb6b04918e6aa305730185462aaf70c
Vilius <vilius.kazlauskas@bentley.com>
2022-05-03 14:03:32 +0300

    moved function

--- packages/iTwinUI-react/src/core/Table/Table.tsx
+++ packages/iTwinUI-react/src/core/Table/Table.tsx
@@ -244,6 +244,22 @@ export type TableProps<
   enableColumnReordering?: boolean;
 } & Omit<CommonProps, 'title'>;
 
+// Original type for some reason is missing sub-columns
+type ColumnType<T extends Record<string, unknown> = Record<string, unknown>> =
+  Column<T> & {
+    columns: ColumnType[];
+  };
+const flattenColumns = (columns: ColumnType[]): ColumnType[] => {
+  const flatColumns: ColumnType[] = [];
+  columns.forEach((column) => {
+    flatColumns.push(column);
+    if (column.columns) {
+      flatColumns.push(...flattenColumns(column.columns));
+    }
+  });
+  return flatColumns;
+};
+
 /**
  * Table based on [react-table](https://react-table.tanstack.com/docs/api/overview).
  * @example
@@ -351,27 +367,10 @@ export const Table = <
     onRowInViewportRef.current = onRowInViewport;
   }, [onBottomReached, onRowInViewport]);
 
-  // Original type for some reason is missing sub-columns
-  type ColumnType = Column<T> & {
-    columns: ColumnType[];
-  };
-  const flattenColumns = React.useCallback(
-    (columns: ColumnType[]): ColumnType[] => {
-      const flatColumns: ColumnType[] = [];
-      columns.forEach((column) => {
-        flatColumns.push(column);
-        if (column.columns) {
-          flatColumns.push(...flattenColumns(column.columns));
-        }
-      });
-      return flatColumns;
-    },
-    [],
-  );
   const hasManualSelectionColumn = React.useMemo(() => {
     const flatColumns = flattenColumns(columns as ColumnType[]);
     return flatColumns.some((column) => column.id === SELECTION_CELL_ID);
-  }, [columns, flattenColumns]);
+  }, [columns]);
 
   const tableStateReducer = React.useCallback(
     (
