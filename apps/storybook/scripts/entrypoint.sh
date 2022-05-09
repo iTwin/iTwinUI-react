df84b58f9141bcae80b1f3a89f2aba0d0a62be10
Mayank <mayank99@users.noreply.github.com>
2022-05-09 15:23:12 -0400

    Add `serve` in dev deps instead of reinstalling

--- apps/storybook/scripts/entrypoint.sh
+++ apps/storybook/scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-yarn global add serve
-
 # `&` is used to run the process in the background
-serve apps/storybook/storybook-static --listen 6006 &
+yarn serve apps/storybook/storybook-static --listen 6006 &
 yarn workspace storybook cypress run "$@"
