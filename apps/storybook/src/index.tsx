3c214afd1221e0b2b8a755b7406df21a2f20a80f
Mayank <mayank99@users.noreply.github.com>
2022-05-09 15:39:09 -0400

    Combine vite shell into `index.html`

--- apps/storybook/src/index.tsx
+++
@@ -1,6 +0,0 @@
-// This file is required for CRA. It deliberately doesn't do anything meaningful.
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<main />, document.getElementById('root'));
