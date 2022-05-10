d6f838ac8907430d43fd5e8fc16b8d96763bd754
Mayank <mayank99@users.noreply.github.com>
2022-05-10 13:22:33 -0400

    Fix Select tests

--- apps/storybook/src/Select.test.ts
+++ apps/storybook/src/Select.test.ts
@@ -19,9 +19,27 @@ describe('Select', () => {
 
     it(testName, () => {
       cy.visit('iframe', { qs: { id } });
+
+      if (testName.includes('Disabled')) {
+        cy.compareSnapshot(testName);
+        return;
+      }
+
+      if (testName.includes('With Selected Value')) {
+        cy.compareSnapshot(`${testName} (Open)`);
+        cy.get('.iui-select-button').click();
+        cy.compareSnapshot(`${testName} (Closed)`);
+        return;
+      }
+
       cy.compareSnapshot(`${testName} (Closed)`);
       cy.get('.iui-select-button').click();
       cy.compareSnapshot(`${testName} (Open)`);
+
+      if (testName.includes('Custom')) {
+        cy.get('.iui-menu-item').first().click();
+        cy.compareSnapshot(`${testName} (Closed With Value)`);
+      }
     });
   });
 });
