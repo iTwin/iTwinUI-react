2b5e936c275bb1a373b3e57ea245d10c59811574
veekeys <81580355+veekeys@users.noreply.github.com>
2022-05-24 16:19:49 +0300

    magic is real

--- apps/storybook/src/Table.stories.tsx
+++ apps/storybook/src/Table.stories.tsx
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React, { useCallback } from 'react';
-import {
+import type {
   actions,
   ActionType,
   CellProps,
