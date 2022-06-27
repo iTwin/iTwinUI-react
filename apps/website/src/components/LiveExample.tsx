78ead90c8561c27e821cabf82207e149f73e5f37
Mayank <mayank99@users.noreply.github.com>
2022-06-27 11:34:05 -0400

    Add prettier config

--- apps/website/src/components/LiveExample.tsx
+++ apps/website/src/components/LiveExample.tsx
@@ -7,7 +7,7 @@ import {
   SandpackThemeProvider,
   SandpackCodeEditor,
   SandpackPreview,
-} from "@codesandbox/sandpack-react";
+} from '@codesandbox/sandpack-react';
 
 const indexJs = `
 import { createRoot } from 'react-dom/client';
@@ -33,24 +33,24 @@ html { color-scheme: dark; }
 `;
 // background: linear-gradient(225deg, #4e5d6e 0%, #363944 100%);
 
-export const LiveExample = ({ code = "", ...rest }) => {
+export const LiveExample = ({ code = '', ...rest }) => {
   // TODO: replace this with composition to have more control over the layout and styling
   return (
     <SandpackProvider
-      template="react"
+      template='react'
       files={{
-        "/App.js": code.trim(),
-        "/index.js": { code: indexJs.trim(), hidden: true },
-        "/styles.css": { code: indexCss.trim(), hidden: true },
+        '/App.js': code.trim(),
+        '/index.js': { code: indexJs.trim(), hidden: true },
+        '/styles.css': { code: indexCss.trim(), hidden: true },
       }}
       customSetup={{
         dependencies: {
-          "@itwin/itwinui-react": "latest",
+          '@itwin/itwinui-react': 'latest',
         },
       }}
       {...rest}
     >
-      <SandpackThemeProvider theme="dark">
+      <SandpackThemeProvider theme='dark'>
         <SandpackPreview />
         <SandpackCodeEditor showTabs={false} />
       </SandpackThemeProvider>
--- apps/website/src/components/SidebarToggle.tsx
+++ apps/website/src/components/SidebarToggle.tsx
@@ -2,42 +2,42 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const MenuToggle = () => {
   const [sidebarShown, setSidebarShown] = useState(false);
 
   // TODO: make this work without useEffect. Move into Astro file for better styling?
   useEffect(() => {
-    const body = document.getElementsByTagName("body")[0];
+    const body = document.getElementsByTagName('body')[0];
     if (sidebarShown) {
-      body.classList.add("mobile-sidebar-toggle");
+      body.classList.add('mobile-sidebar-toggle');
     } else {
-      body.classList.remove("mobile-sidebar-toggle");
+      body.classList.remove('mobile-sidebar-toggle');
     }
   }, [sidebarShown]);
 
   return (
     <button
-      type="button"
-      aria-pressed={sidebarShown ? "true" : "false"}
-      id="menu-toggle"
+      type='button'
+      aria-pressed={sidebarShown ? 'true' : 'false'}
+      id='menu-toggle'
       onClick={() => setSidebarShown(!sidebarShown)}
-      aria-label="Toggle sidebar"
+      aria-label='Toggle sidebar'
     >
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+        xmlns='http://www.w3.org/2000/svg'
+        width='1em'
+        height='1em'
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
       >
         <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          strokeWidth='2'
+          d='M4 6h16M4 12h16M4 18h16'
         />
       </svg>
     </button>
