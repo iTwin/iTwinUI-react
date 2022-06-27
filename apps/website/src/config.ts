03e6ff0cc6c68eb5aca1ef65b265349c1ab004df
Mayank <mayank99@users.noreply.github.com>
2022-06-27 11:32:55 -0400

    Rename components

--- apps/website/src/config.ts
+++ apps/website/src/config.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 export const SITE = {
   title: 'iTwinUI',
   description: 'An open source design system for building consistent interfaces',
@@ -15,11 +19,13 @@ export const SIDEBAR = [
   { text: 'Introduction', link: 'docs/introduction' },
   { text: 'Components', header: true },
   { text: 'Alert', link: 'docs/alert' },
+  { text: 'Avatar', link: 'docs/avatar' },
   { text: 'Badge', link: 'docs/badge' },
   { text: 'Breadcrumbs', link: 'docs/breadcrumbs' },
   { text: 'Carousel', link: 'docs/carousel' },
   { text: 'ColorPicker', link: 'docs/combobox' },
   { text: 'DatePicker', link: 'docs/datepicker' },
+  { text: 'Dialog', link: 'docs/dialog' },
   { text: 'DropdownMenu', link: 'docs/dropdownmenu' },
   { text: 'ErrorPage', link: 'docs/errorpage' },
   { text: 'ExpandableBlock', link: 'docs/expandableblock' },
@@ -27,9 +33,9 @@ export const SIDEBAR = [
   { text: 'Footer', link: 'docs/footer' },
   { text: 'Header', link: 'docs/header' },
   { text: 'InformationPanel', link: 'docs/informationpanel' },
-  { text: 'Modal', link: 'docs/modal' },
   { text: 'SideNavigation', link: 'docs/sidenavigation' },
   { text: 'SkipToContentLink', link: 'docs/skiptocontentlink' },
+  { text: 'Stepper', link: 'docs/stepper' },
   { text: 'Surface', link: 'docs/surface' },
   { text: 'Table', link: 'docs/table' },
   { text: 'Tabs', link: 'docs/tabs' },
@@ -37,10 +43,8 @@ export const SIDEBAR = [
   { text: 'Tile', link: 'docs/tile' },
   { text: 'TimePicker', link: 'docs/timepicker' },
   { text: 'Toast', link: 'docs/toast' },
-  { text: 'tooltip', link: 'docs/tooltip' },
+  { text: 'Tooltip', link: 'docs/tooltip' },
   { text: 'Tree', link: 'docs/tree' },
-  { text: 'UserIcon', link: 'docs/usericon' },
-  { text: 'Wizard', link: 'docs/wizard' },
 
   { text: 'Another Section', header: true },
   { text: 'Blah blah', link: 'docs/blah' },
