6c11ad94e4ec44728677bbf7b631525f01bb947e
Mayank <mayank99@users.noreply.github.com>
2022-06-30 16:18:53 -0400

    Improve TOC (WIP)

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -12,7 +12,50 @@ export default defineConfig({
     rehypePlugins: [
       'rehype-slug',
       ['rehype-autolink-headings', { behavior: 'wrap' }],
-      ['rehype-toc', { headings: ['h1', 'h2', 'h3'] }],
+      [
+        'rehype-toc',
+        {
+          headings: ['h1', 'h2', 'h3'],
+          customizeTOC: addOverviewToTOC,
+        },
+      ],
     ],
   },
 });
+
+/**
+ * Adds "Overview" link at the top of the table of contents.
+ * This is useful for scrolling to the introduction section
+ * that is not under any heading.
+ */
+function addOverviewToTOC(nav) {
+  const ol = nav.children[0];
+  if (ol.children.length === 0) {
+    return false;
+  }
+
+  return {
+    ...nav,
+    children: [
+      {
+        ...ol,
+        children: [
+          {
+            type: 'element',
+            tagName: 'li',
+            properties: { className: 'toc-item toc-item-h2' },
+            children: [
+              {
+                type: 'element',
+                tagName: 'a',
+                properties: { href: '#overview', className: 'toc-link toc-link-h1' },
+                children: [{ type: 'text', value: 'Overview' }],
+              },
+            ],
+          },
+          ...ol.children,
+        ],
+      },
+    ],
+  };
+}
