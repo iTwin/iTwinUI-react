573648c37f328b268d1b972c92818ad139d36603
Greta <greta.nausedaite@bentley.com>
2022-07-29 15:26:12 +0300

    Updating logo

--- apps/website/src/components/iTwinUILogo.tsx
+++ apps/website/src/components/iTwinUILogo.tsx
@@ -8,6 +8,7 @@ export const ITwinUILogo = () => {
   const [coords, setCoords] = React.useState({ x: 0, y: 0 });
   const [rotateDeg, setRotateDeg] = React.useState({ x: 0, y: 0 });
   const [animation, setAnimation] = React.useState('none');
+  const [degrees, setDegrees] = React.useState(0);
 
   const handleMouseMove = (event) => {
     const rect = event.target.getBoundingClientRect();
@@ -19,6 +20,18 @@ export const ITwinUILogo = () => {
       x: (event.clientX - rect.left - rect.width / 2) / 20,
       y: -(event.clientY - rect.top - rect.height / 2) / 20,
     });
+
+    // TODO: figure out how to calculate degrees based on mouse location
+    setDegrees(
+      Math.abs(
+        Math.atan(
+          (event.clientX - rect.left - rect.width / 2) /
+            -(event.clientY - rect.top - rect.height / 2)
+        ) *
+          (180 / Math.PI)
+      )
+    );
+    console.log(degrees);
   };
 
   const handleMouseEnter = () => {
@@ -31,22 +44,18 @@ export const ITwinUILogo = () => {
     setCoords({ x: 0, y: 0 });
   };
 
+  // TODO: figure out how to style in css files, not inline
   return (
     <div
       style={{
-        width: '300px',
+        width: '350px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '300px',
-        background: `linear-gradient(
-          35deg,hsl(314, 57%, 95%),hsl(232, 79%, 90%),hsl(186, 78%, 85%), #fff ${
-            50 + rotateDeg.x * rotateDeg.y * 0.5
-          }%,hsl(232, 79%, 95%),hsl(314, 57%, 90%),hsl(186, 78%, 89%) )`,
+        height: '350px',
         transform: `rotateY(${rotateDeg.x}deg) rotateX(${rotateDeg.y}deg)`,
         borderRadius: '25%',
         transformStyle: 'preserve-3d',
-        boxShadow: `rgba(255, 255, 255,0.5) 0px 5px 5px inset, rgba(0, 0, 0,0.5) 0px -1px 6px inset`,
         transition: `${animation}`,
       }}
       onMouseMove={handleMouseMove}
@@ -54,12 +63,29 @@ export const ITwinUILogo = () => {
       onMouseLeave={handleMouseLeave}
     >
       <svg
+        id='Layer_2'
         xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 174 174'
-        fill='#293246'
-        style={{ height: '340px' }}
+        viewBox='0 0 297.5 340'
+        style={{ height: '90%' }}
       >
-        <path d='m8.39529 16c-.17462 0-.34827-.04625-.50466-.13777-.31565-.18795-.50466-.52253-.50466-.89746v-5.5677c0-.10559-.05568-.20336-.14649-.25724l-.93575-.55518c-.29167-.17713-.38857-.56337-.21587-.86253.17078-.29866.54496-.40149.84142-.2219l1.09279.66227c.3675.22272.59197.62122.59197 1.05094v5.18002c0 .08056.08773.13046.15697.08928l4.86593-2.89371c.09082-.05401.1464-.15193.14622-.25759l-.01094-6.49616c-.00018-.10574-.05617-.20353-.14727-.2572l-5.48384-3.23106c-.0959-.0565-.21496-.05627-.31064.00061l-5.46736 3.25009c-.09084.054-.14645.15193-.14627.25762l.01094 6.49695c.00018.10556.05599.20321.14685.25695l3.97323 2.34984c.29359.17516.39337.55993.22355.86105-.17078.30112-.54687.40051-.8395.22929l-4.11958-2.45022c-.37967-.22582-.61233-.6349-.61233-1.07664v-6.85789c0-.44262.23367-.85237.61463-1.07774l5.78573-3.42285c.36978-.21876.82936-.21877 1.19915 0l5.79701 3.42944c.37405.22128.60348.62359.60348 1.05818v6.88462c0 .43325-.22821.83446-.6006 1.0559l-5.49753 3.26903c-.15639.0925-.331.13875-.50658.13875zm-1.25517-9.60988c0 .48696.38488.88171.85965.88171s.85965-.39476.85965-.88171-.38488-.88171-.85965-.88171-.85965.39476-.85965.88171z' />
+        <defs>
+          {/** Move grandients into css, this one does not allow more than 90deg rotation. Maybe move into astro file? */}
+          <linearGradient id='grad1' gradientTransform={`rotate(45)`}>
+            <stop offset='0%' style={{ stopColor: '#ffb6fc', stopOpacity: '1' }} />
+            <stop offset='100%' style={{ stopColor: '#83a4ff', stopOpacity: '1' }} />
+          </linearGradient>
+          <linearGradient id='gra2' gradientTransform={`rotate(90)`}>
+            <stop offset='0%' style={{ stopColor: '#fff', stopOpacity: '1' }} />
+            <stop offset='50%' style={{ stopColor: '#fff', stopOpacity: '0' }} />
+            <stop offset='100%' style={{ stopColor: '#fff', stopOpacity: '1' }} />
+          </linearGradient>
+        </defs>
+        <path
+          stroke='url(#gra2)'
+          strokeWidth='2px'
+          fill='url(#grad1)'
+          d='M157.15,340c-3.71,0-7.4-.98-10.72-2.93-6.71-3.99-10.72-11.1-10.72-19.07v-118.31c0-2.24-1.18-4.32-3.11-5.47l-19.88-11.8c-6.2-3.76-8.26-11.97-4.59-18.33,3.63-6.35,11.58-8.53,17.88-4.72l23.22,14.07c7.81,4.73,12.58,13.2,12.58,22.33v110.08c0,1.71,1.86,2.77,3.34,1.9l103.4-61.49c1.93-1.15,3.11-3.23,3.11-5.47l-.23-138.04c0-2.25-1.19-4.33-3.13-5.47L151.75,28.62c-2.04-1.2-4.57-1.2-6.6,.01L28.97,97.7c-1.93,1.15-3.11,3.23-3.11,5.47l.23,138.06c0,2.24,1.19,4.32,3.12,5.46l84.43,49.93c6.24,3.72,8.36,11.9,4.75,18.3-3.63,6.4-11.62,8.51-17.84,4.87L13.01,267.73c-8.07-4.8-13.01-13.49-13.01-22.88V99.12c0-9.41,4.97-18.11,13.06-22.9L136.01,3.49c7.86-4.65,17.62-4.65,25.48,0l123.19,72.88c7.95,4.7,12.82,13.25,12.82,22.49V245.15c0,9.21-4.85,17.73-12.76,22.44l-116.82,69.47c-3.32,1.97-7.03,2.95-10.76,2.95Zm-26.67-204.21c0,10.35,8.18,18.74,18.27,18.74s18.27-8.39,18.27-18.74-8.18-18.74-18.27-18.74-18.27,8.39-18.27,18.74Z'
+        />
       </svg>
     </div>
   );
