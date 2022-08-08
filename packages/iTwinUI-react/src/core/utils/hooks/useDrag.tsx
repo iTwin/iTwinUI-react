550f11764a424c2a8c5187807876b10d8326f7fd
Vilius <vilius.kazlauskas@bentley.com>
2022-08-08 17:24:19 +0300

    feat(Dialog): Draggable Dialog

---
+++ packages/iTwinUI-react/src/core/utils/hooks/useDrag.tsx
@@ -0,0 +1,65 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import React from 'react';
+
+export const useDrag = (elementRef: React.RefObject<HTMLElement>) => {
+  const grabOffsetX = React.useRef(0);
+  const grabOffsetY = React.useRef(0);
+  const translateX = React.useRef(0);
+  const translateY = React.useRef(0);
+
+  const [transform, setTransform] = React.useState('');
+
+  const onPointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+    if (!elementRef.current || e.button !== 0) {
+      return;
+    }
+    console.log('onPointerDown');
+    const transformValue = getComputedStyle(
+      elementRef.current,
+    ).getPropertyValue('transform');
+    const matrix = new DOMMatrix(transformValue);
+
+    translateX.current = matrix.m41;
+    translateY.current = matrix.m42;
+
+    grabOffsetX.current = e.clientX - translateX.current;
+    grabOffsetY.current = e.clientY - translateY.current;
+
+    elementRef.current.ownerDocument.addEventListener(
+      'pointermove',
+      onPointerMove,
+      false,
+    );
+    elementRef.current.ownerDocument.addEventListener(
+      'pointerup',
+      () => {
+        if (elementRef.current) {
+          setTransform(
+            `translate(${translateX.current}px, ${translateY.current}px)`,
+          );
+        }
+        document.removeEventListener('pointermove', onPointerMove);
+      },
+      { once: true },
+    );
+  };
+
+  const onPointerMove = React.useCallback(
+    (event: PointerEvent) => {
+      if (elementRef.current) {
+        console.log('onPointerMove');
+        translateX.current = event.clientX - grabOffsetX.current;
+        translateY.current = event.clientY - grabOffsetY.current;
+        elementRef.current.style.transform = `translate(${translateX.current}px, ${translateY.current}px)`;
+      }
+    },
+    [elementRef],
+  );
+
+  return { onPointerDown, transform };
+};
+
+export default useDrag;
