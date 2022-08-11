29a2199480e2d37146cbfce09cd885d9c895e09b
veekeys <81580355+veekeys@users.noreply.github.com>
2022-08-11 16:55:39 +0300

    Change script to build single platform image according to machine

--- apps/storybook/scripts/build-docker.js
+++ apps/storybook/scripts/build-docker.js
@@ -2,16 +2,21 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
 const dockerfilePath = path.join(__dirname, 'docker');
-
+const platform = os.arch().includes('arm') ? 'linux/arm64/v8' : 'linux/amd64';
+const binaryPath = os.arch().includes('arm')
+  ? '/tmp/cypress-build/linux/build/linux-arm64-unpacked'
+  : '/tmp/cypress-build/linux/build/linux-unpacked';
+const imagePlatformName = os.arch().includes('arm') ? 'arm' : 'amd';
 // Change this before new image push
 const VERSION = '1.0.0';
 
 spawn(
-  `docker buildx build ${dockerfilePath} --platform linux/arm64/v8,linux/amd64 -t bentleysystemsinc/itwinui-cypress:latest -t bentleysystemsinc/itwinui-cypress:${VERSION} --push`,
+  `docker buildx build ${dockerfilePath} --platform ${platform} --build-arg BINARY_PATH=${binaryPath} -t bentleysystemsinc/itwinui-cypress:${imagePlatformName} -t bentleysystemsinc/itwinui-cypress:${VERSION}-${imagePlatformName} --push`,
   {
     stdio: 'inherit',
     shell: true,
