99a22dfc456ec6fa3d51c6b90b65b815a08a0d72
Greta <greta.nausedaite@bentley.com>
2022-08-18 17:50:11 +0300

    home page update

--- apps/website/src/demos/Slider.demo.tsx
+++ apps/website/src/demos/Slider.demo.tsx
@@ -2,6 +2,9 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export { default as DialogDemo } from './Dialog.demo';
-export { default as FieldsetDemo } from './Fieldset.demo';
-export { default as SliderDemo } from './Slider.demo';
+import * as React from 'react';
+import { Slider } from '@itwin/itwinui-react';
+
+export default () => {
+  return <Slider style={{ width: '80%' }} values={[10, 30]} min={0} max={50} />;
+};
