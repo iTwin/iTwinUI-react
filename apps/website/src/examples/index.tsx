8d142f7ab4e0b706c95ea3f8875f19b31117c185
Greta <greta.nausedaite@bentley.com>
2022-09-01 13:52:00 +0300

    Added more examples

--- apps/website/src/examples/index.tsx
+++ apps/website/src/examples/index.tsx
@@ -5,6 +5,7 @@
 export { default as AlertMainExample } from './Alert.main';
 export { default as AvatarMainExample } from './Avatar.main';
 export { default as BadgeMainExample } from './Badge.main';
+export { default as BlockquoteMainExample } from './Blockquote.main';
 export { default as BreadcrumbsMainExample } from './Breadcrumbs.main';
 export { default as CarouselMainExample } from './Carousel.main';
 export { default as ComboBoxMainExample } from './ComboBox.main';
@@ -21,6 +22,9 @@ export { default as InformationPanelMainExample } from './InformationPanel.main'
 export { default as SideNavigationMainExample } from './SideNavigation.main';
 export { default as ToastStaticExample } from './Toast.static';
 export { default as ToastMainExample } from './Toast.main';
+export { default as TableMainExample } from './Table.main';
 export { default as TileMainExample } from './Tile.main';
 export { default as TooltipMainExample } from './Tooltip.main';
 export { default as TooltipStaticExample } from './Tooltip.static';
+export { default as TreeMainExample } from './Tree.main';
+export { default as WizardMainExample } from './Wizard.main';
