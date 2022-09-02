756948dfa64835d05afa992a85624f2798d48a7c
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-09-02 15:14:33 -0400

    Cleaned up code to make PR ready for review

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -2,61 +2,10 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { ActionColumn, Button, Table } from '@itwin/itwinui-react';
-import React from 'react';
-
-const mockTableData = Array.from(new Array(1000).keys()).map((k) => {
-  return {
-    id: `opa${k}`,
-  };
-});
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
-  const columns = React.useMemo(
-    () => [
-      {
-        ['Header']: 'TableData',
-        columns: [
-          {
-            ...ActionColumn({ columnManager: true }),
-          },
-          {
-            ['Header']: 'id',
-            id: 'id',
-            accessor: 'id',
-            width: 100,
-            disableSortBy: true,
-          },
-        ],
-      },
-    ],
-    [],
-  );
-
-  const tableData = mockTableData;
-  const tableRef = React.useRef<HTMLDivElement>(null);
-
-  const show = () => {
-    const divElement = tableRef.current;
-    divElement?.setAttribute('style', 'display: block;');
-  };
-
-  return (
-    <div>
-      <Button onClick={show}>"Show"</Button>
-      <div ref={tableRef} className='parent'>
-        <Table
-          columns={columns}
-          data={tableData}
-          emptyTableContent='No data.'
-          isLoading={false}
-          enableVirtualization={true}
-          density='extra-condensed'
-          style={{ height: '300px' }}
-        />
-      </div>
-    </div>
-  );
+  return <Button>Hello world</Button>;
 };
 
 export default App;
