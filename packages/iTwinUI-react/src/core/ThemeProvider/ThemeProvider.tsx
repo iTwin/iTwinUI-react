8ea09dbaca0324f1af5d50450642c16cc67ab843
Mayank <mayank99@users.noreply.github.com>
2022-09-12 15:57:55 -0400

    feat: Allow scoped styles using `ThemeProvider`

--- packages/iTwinUI-react/src/core/ThemeProvider/ThemeProvider.tsx
+++ packages/iTwinUI-react/src/core/ThemeProvider/ThemeProvider.tsx
@@ -3,31 +3,77 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
-import { ThemeOptions, ThemeType, useTheme } from '../utils';
+import cx from 'classnames';
+import {
+  PolymorphicComponentProps,
+  PolymorphicForwardRefComponent,
+  useMediaQuery,
+} from '../utils';
+import {
+  useTheme,
+  ThemeOptions,
+  ThemeType,
+  UseThemeProps,
+} from '../utils/hooks/useTheme';
 
-export type ThemeProviderProps = {
-  /**
-   * Theme to be applied. If not set, light theme will be used.
-   */
-  theme?: ThemeType;
-  /**
-   * Optional children.
-   */
-  children?: React.ReactNode;
-  /**
-   * Options that can be specified to override default theming behavior.
-   */
-  themeOptions?: ThemeOptions;
-};
+export type ThemeProviderProps<
+  T extends React.ElementType = 'div'
+> = PolymorphicComponentProps<T, UseThemeProps>;
 
 /**
  * Component providing global styles that are required for all components and allows changing theme.
  */
-export const ThemeProvider = (props: ThemeProviderProps) => {
-  const { theme, children, themeOptions } = props;
+export const ThemeProvider = React.forwardRef((props, ref) => {
+  const {
+    theme,
+    children,
+    themeOptions,
+    as: Element = 'div',
+    className,
+    ...rest
+  } = props;
 
-  useTheme(theme, themeOptions);
-  return <>{children}</>;
-};
+  const hasChildren = React.Children.count(children) > 0;
+  const parentContext = React.useContext(ThemeContext);
+  const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersHighContrast = useMediaQuery('(prefers-contrast: more)');
+
+  const shouldApplyDark = theme === 'dark' || (theme === 'os' && prefersDark);
+  const shouldApplyHC = themeOptions?.highContrast ?? prefersHighContrast;
+
+  // only provide context if wrapped around children
+  return hasChildren ? (
+    <ThemeContext.Provider value={{ theme, themeOptions }}>
+      <Element
+        className={cx('iui-root', className)}
+        data-iui-theme={shouldApplyDark ? 'dark' : 'light'}
+        data-iui-contrast={shouldApplyHC ? 'high' : 'default'}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Element>
+    </ThemeContext.Provider>
+  ) : (
+    // otherwise just apply theme on the root using this wrapper component
+    <ThemeLogicWrapper
+      theme={theme ?? parentContext?.theme}
+      themeOptions={themeOptions ?? parentContext?.themeOptions}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'div', UseThemeProps>;
 
 export default ThemeProvider;
+
+export const ThemeContext = React.createContext<
+  | {
+      theme?: ThemeType;
+      themeOptions?: ThemeOptions;
+    }
+  | undefined
+>(undefined);
+
+const ThemeLogicWrapper = ({ theme, themeOptions }: UseThemeProps) => {
+  useTheme(theme, themeOptions);
+  return <></>;
+};
