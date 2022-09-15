d215320f03fb1a35e8a115bd27c8dc2e40def831
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-09-15 15:26:01 -0400

    Cleaned story

--- apps/storybook/src/NotificationMarker.stories.tsx
+++ apps/storybook/src/NotificationMarker.stories.tsx
@@ -8,10 +8,8 @@ import {
   IconButton,
   NotificationMarker,
   NotificationMarkerProps,
-  // Text,
 } from '@itwin/itwinui-react';
 import SvgNotification from '@itwin/itwinui-icons-react/cjs/icons/Notification';
-// import { action } from '@storybook/addon-actions';
 
 export default {
   component: NotificationMarker,
@@ -25,7 +23,6 @@ export default {
   },
   title: 'Core/NotificationMarker',
   decorators: [
-    // (Story, context) => (
     (Story) => (
       <div
         style={{
@@ -38,10 +35,6 @@ export default {
   ],
 } as Meta<NotificationMarkerProps>;
 
-// const trialP = () => {
-//   return <b className='trial b'>abcd</b>;
-// };
-
 export const Basic: Story<NotificationMarkerProps> = (args) => {
   console.log(args);
   return (
@@ -51,93 +44,9 @@ export const Basic: Story<NotificationMarkerProps> = (args) => {
       </NotificationMarker>
     </IconButton>
   );
-
-  // return (
-  //   <IconButton>
-  //     <NotificationMarker>
-  //       <SvgNotification />
-  //       {/* <b>abc</b> */}
-  //     </NotificationMarker>
-  //   </IconButton>
-  // );
-
-  // return (
-  //   <Button
-  //     styleType='borderless'
-  //     // startIcon={<Text className='trial text class'>trial text</Text>}
-  //     startIcon={
-  //       <NotificationMarker className='My trial classname 1'>
-  //         <p>abc</p>
-  //       </NotificationMarker>
-  //     }
-  //     endIcon={<p className='trial p'>abcd</p>}
-  //   >
-  //     <NotificationMarker className='my trial classname 2'>
-  //       <p>abc</p>
-  //     </NotificationMarker>
-  //   </Button>
-  // );
-
-  // <NotificationMarker>
-  //       {/* <Button>abc</Button> */}
-  //       {/* abc */}
-  //       <SvgNotification />
-  //       {/* abcd */}
-  //     </NotificationMarker>
-
-  // return (
-  //   <IconButton>
-  //     {/* <div className='abcde' /> */}
-  //     <NotificationMarker>
-  //       {/* <div className={'iui-notification-positive iui-urgent'}> */}
-  //       {/* <div className='inner-div' /> */}
-  //       <SvgNotification />
-  //       {/* </div> */}
-  //     </NotificationMarker>
-  //   </IconButton>
-  // );
-
-  // return (
-  //   <IconButton
-  //     key='notif'
-  //     onClick={() => action('Clicked on the notification bell')()}
-  //     styleType='borderless'
-  //   >
-  //     <NotificationMarker>
-  //       <div>
-  //         {/* <SvgNotification /> */}
-  //         {/* anb */}
-  //         {/* <IconButton
-  //         key='notif'
-  //         onClick={() => action('Clicked on the notification bell')()}
-  //         styleType='borderless'
-  //       > */}
-  //         <SvgNotification />
-  //         {/* </IconButton> */}
-  //       </div>
-  //     </NotificationMarker>
-  //   </IconButton>
-  // );
-
-  // return <NotificationMarker {...args} />;
 };
 
 Basic.args = {
   variant: 'primary',
   urgent: false,
 };
-
-// Basic.args = {
-//   // child: <SvgNotification />,
-//   child: (
-//     <IconButton
-//       key='notif'
-//       onClick={() => action('Clicked on the notification bell')()}
-//       styleType='borderless'
-//     >
-//       <SvgNotification>
-//         a
-//       </SvgNotification>
-//     </IconButton>
-//   ),
-// };
