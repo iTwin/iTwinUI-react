041123b2873b9ba3bc3be4cc898c67c99ab6ae4e
adamhock <78564221+adamhock@users.noreply.github.com>
2022-09-21 01:56:28 -0400

    initial commit

--- packages/iTwinUI-react/src/core/Table/TablePaginator.tsx
+++ packages/iTwinUI-react/src/core/Table/TablePaginator.tsx
@@ -114,6 +114,7 @@ export const TablePaginator = (props: TablePaginatorProps) => {
     totalRowsCount,
     pageSize,
     onPageChange,
+    totalSelectedRowsCount,
     focusActivationMode = 'manual',
     isLoading = false,
     size = 'default',
@@ -282,7 +283,15 @@ export const TablePaginator = (props: TablePaginatorProps) => {
       ref={paginatorResizeRef}
       {...rest}
     >
-      <div className='iui-left' />
+      <div className='iui-left'>
+        {totalSelectedRowsCount && totalSelectedRowsCount > 0 && (
+          <span className='iui-table-paginator-selection-count'>
+            {`${totalSelectedRowsCount} ${
+              totalSelectedRowsCount > 1 ? 'rows' : 'row'
+            } selected`}
+          </span>
+        )}
+      </div>
       {showPagesList && (
         <div className='iui-center' ref={overflowRef}>
           <IconButton
