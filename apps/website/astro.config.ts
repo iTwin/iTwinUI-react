34ecfc9266ac4800bade8688500ad2804b1b1190
Mayank <mayank99@users.noreply.github.com>
2022-09-27 12:29:59 -0400

    Add prism highlight

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -14,6 +14,7 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   integrations: [react(), mdx()],
   markdown: {
+    syntaxHighlight: 'prism',
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypeToc],
   },
   vite: {
