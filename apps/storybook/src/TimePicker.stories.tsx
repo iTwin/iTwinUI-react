8891beb29681bc4703786ebfbc1d10e91176dcd6
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2022-10-04 16:58:40 -0500

    Add radio for precision controls

--- apps/storybook/src/TimePicker.stories.tsx
+++ apps/storybook/src/TimePicker.stories.tsx
@@ -10,6 +10,7 @@ import {
   TimePickerProps,
   IconButton,
   LabeledInput,
+  MeridiemType,
 } from '@itwin/itwinui-react';
 import SvgCalendar from '@itwin/itwinui-icons-react/cjs/icons/Calendar';
 
@@ -22,8 +23,12 @@ export default {
     style: { control: { disable: true } },
     id: { control: { disable: true } },
     date: { control: { type: 'date' } },
+    precision: {
+      control: { type: 'radio' },
+      options: ['hours', 'minutes', 'seconds'],
+    },
   },
-  args: { use12Hours: false },
+  args: { use12Hours: false, precision: 'minutes' },
 } as Meta<TimePickerProps>;
 
 export const Basic: Story<TimePickerProps> = (args) => {
@@ -100,6 +105,9 @@ export const CustomRenderers: Story<TimePickerProps> = (args) => {
           : `${date.getMinutes()} mins`}
       </>
     ),
+    meridiemRenderer = (meridiem: MeridiemType) => (
+      <>{meridiem === 'AM' ? 'Before' : 'After'}</>
+    ),
     ...rest
   } = args;
   const [opened, setOpened] = React.useState(false);
@@ -140,6 +148,7 @@ export const CustomRenderers: Story<TimePickerProps> = (args) => {
             use12Hours={use12Hours}
             hourRenderer={hourRenderer}
             minuteRenderer={minuteRenderer}
+            meridiemRenderer={meridiemRenderer}
           />
         </div>
       )}
@@ -163,6 +172,9 @@ CustomRenderers.args = {
         ? `${date.getMinutes()} min`
         : `${date.getMinutes()} mins`}
     </>
+  ),
+  meridiemRenderer: (meridiem: MeridiemType) => (
+    <>{meridiem === 'AM' ? 'Before' : 'After'}</>
   ),
 };
 
