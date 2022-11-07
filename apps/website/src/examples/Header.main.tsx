dad18090a3b050f51c06d2d0a089a42b298c8465
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-11-07 07:55:15 -0500

    Undid formatting change in unrelated file

--- apps/website/src/examples/Header.main.tsx
+++ apps/website/src/examples/Header.main.tsx
@@ -19,6 +19,8 @@ import {
 } from '@itwin/itwinui-react';
 
 export default () => {
+  console.log('dummy change');
+
   const buildMenu = (menu: string) => (close: () => void) =>
     [
       <MenuItem key={1} value={'Item #1'} onClick={close}>
