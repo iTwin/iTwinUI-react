2ebbecfef60e00bd1a625a1635cb973b9d955340
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-11-10 09:23:00 -0500

    Added express

--- apps/migration-backend/app.js
+++ apps/migration-backend/app.js
@@ -2,25 +2,39 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-const http = require('http');
-const fs = require('fs');
-const port = 5000;
+// /*---------------------------------------------------------------------------------------------
+//  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+//  * See LICENSE.md in the project root for license terms and full copyright notice.
+//  *--------------------------------------------------------------------------------------------*/
+// const http = require('http');
+// const fs = require('fs');
+// const port = 5000;
 
-const server = http.createServer((req, res) => {
-  fs.readFile('to_read.txt', (err, data) => {
-    if (err) {
-      res.write('ERROR', err);
-    } else {
-      res.write(`Worked! ${data}`);
-    }
-    res.end();
-  });
+// const server = http.createServer((req, res) => {
+//   fs.readFile('to_read.txt', (err, data) => {
+//     if (err) {
+//       res.write('ERROR', err);
+//     } else {
+//       res.write(`Worked! ${data}`);
+//     }
+//     res.end();
+//   });
+// });
+
+// server.listen(port, (err) => {
+//   if (err) {
+//     console.log('ERROR STARTING SERVER', err);
+//   } else {
+//     console.log('Server started!');
+//   }
+// });
+
+import Express from 'express';
+
+const app = Express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
-server.listen(port, (err) => {
-  if (err) {
-    console.log('ERROR STARTING SERVER', err);
-  } else {
-    console.log('Server started!');
-  }
-});
+app.listen(5000, () => console.log('Running on port 5000'));
