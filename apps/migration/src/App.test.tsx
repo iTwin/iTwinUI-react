cce0aaa1e898c8ea6f964e7532fd536d00817627
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-11-10 03:43:15 -0500

    create-react-app

---
+++ apps/migration/src/App.test.tsx
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
