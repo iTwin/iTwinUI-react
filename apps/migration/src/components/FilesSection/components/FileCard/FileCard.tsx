2a402af6176563a4593179ae6868b96eeb13860b
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-11-10 06:13:46 -0500

    Made FileCard into ExpandableBlock from Surface

--- apps/migration/src/components/FilesSection/components/FileCard/FileCard.tsx
+++ apps/migration/src/components/FilesSection/components/FileCard/FileCard.tsx
@@ -2,17 +2,32 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Surface, Text } from '@itwin/itwinui-react';
+import { ExpandableBlock, Surface, Text } from '@itwin/itwinui-react';
 import './FileCard.scss';
+import SvgStatusSuccess from '@itwin/itwinui-icons-react/cjs/icons/StatusSuccess';
 import { LineChange } from './components/LineChange/LineChange';
 
 export const FileCard = () => {
+  const StatusIcon = () => (
+    <SvgStatusSuccess className='status-icon' fill='green' aria-hidden />
+  );
+
   return (
-    <Surface className='FileCard' elevation={2}>
-      <Text variant='title'>.../src/App.tsx</Text>
+    <ExpandableBlock
+      title={
+        <span className='header'>
+          {StatusIcon()}
+          <Text className={'title'} variant='leading'>
+            .../src/App.tsx
+          </Text>
+        </span>
+      }
+      className='FileCard'
+      isExpanded={true}
+    >
       {[...Array(5)].map(() => {
         return <LineChange />;
       })}
-    </Surface>
+    </ExpandableBlock>
   );
 };
