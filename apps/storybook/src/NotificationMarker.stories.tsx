c4152ddbca9e063bc1a09dd2e9182e3fe6d0e715
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-12-07 18:45:21 -0500

    Re-added old NotificationMarker stories

--- apps/storybook/src/NotificationMarker.stories.tsx
+++ apps/storybook/src/NotificationMarker.stories.tsx
@@ -5,19 +5,130 @@
 import { Story, Meta } from '@storybook/react';
 import React from 'react';
 import {
+  Text,
+  IconButton,
   NotificationMarker,
   NotificationMarkerProps,
 } from '@itwin/itwinui-react';
+import SvgNotification from '@itwin/itwinui-icons-react/cjs/icons/Notification';
 
 export default {
   component: NotificationMarker,
+  args: {
+    enabled: true,
+    pulsing: false,
+  },
   argTypes: {
     className: { control: { disable: true } },
     style: { control: { disable: true } },
+    status: {
+      control: 'radio',
+      options: ['primary', 'positive', 'warning', 'negative'],
+    },
   },
   title: 'Core/NotificationMarker',
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          gap: '20px',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta<NotificationMarkerProps>;
 
 export const Basic: Story<NotificationMarkerProps> = (args) => {
-  return <NotificationMarker {...args} />;
+  return (
+    <>
+      <IconButton styleType='borderless'>
+        <NotificationMarker {...args}>{args.children}</NotificationMarker>
+      </IconButton>
+      <NotificationMarker {...args}>Normal</NotificationMarker>
+    </>
+  );
+};
+
+Basic.args = {
+  status: 'primary',
+  children: <SvgNotification />,
+};
+
+Basic.argTypes = {
+  children: {
+    control: false,
+  },
+};
+
+export const Pulsing: Story<NotificationMarkerProps> = (args) => {
+  return (
+    <>
+      <IconButton styleType='borderless'>
+        <NotificationMarker status='primary' {...args}>
+          <SvgNotification />
+        </NotificationMarker>
+      </IconButton>
+
+      <NotificationMarker status='primary' {...args}>
+        <Text style={{ fontWeight: '700' }}>URGENT</Text>
+      </NotificationMarker>
+    </>
+  );
+};
+
+Pulsing.args = {
+  status: 'negative',
+  pulsing: true,
+};
+
+export const Status: Story<NotificationMarkerProps> = (args) => {
+  return (
+    <>
+      <span>
+        <IconButton styleType='borderless'>
+          <NotificationMarker status='primary' {...args}>
+            <SvgNotification />
+          </NotificationMarker>
+        </IconButton>
+        <IconButton styleType='borderless'>
+          <NotificationMarker status='positive' {...args}>
+            <SvgNotification />
+          </NotificationMarker>
+        </IconButton>
+        <IconButton styleType='borderless'>
+          <NotificationMarker status='warning' {...args}>
+            <SvgNotification />
+          </NotificationMarker>
+        </IconButton>
+        <IconButton styleType='borderless'>
+          <NotificationMarker status='negative' {...args}>
+            <SvgNotification />
+          </NotificationMarker>
+        </IconButton>
+      </span>
+      <NotificationMarker status='primary' {...args}>
+        Primary
+      </NotificationMarker>
+      <NotificationMarker status='positive' {...args}>
+        Positive
+      </NotificationMarker>
+      <NotificationMarker status='warning' {...args}>
+        Warning
+      </NotificationMarker>
+      <NotificationMarker status='negative' {...args}>
+        Negative
+      </NotificationMarker>
+    </>
+  );
+};
+
+Status.argTypes = {
+  status: {
+    control: false,
+  },
 };
