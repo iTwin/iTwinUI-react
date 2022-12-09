aaa835fdcd83987dc8399e9de16c0ae58facab10
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-12-08 20:17:05 -0500

    Reverted playground changes

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,33 +1,9 @@
-import { Button, Table } from '@itwin/itwinui-react';
-import { useState } from 'react';
-import { useColumns } from './useColumns';
-import { useData } from './useData';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
-  const [customColumns, setCustomColumns] = useState<boolean>(true);
-
-  const columns = useColumns(customColumns);
-  const data = useData(10);
-
-  // console.log(
-  //   'passed columns',
-  //   columns.map((c) => c.id),
-  // );
-
   return (
     <>
-      <Button onClick={() => setCustomColumns((cur) => !cur)}>
-        Toggle columns
-      </Button>
-      <pre>
-        {/* {JSON.stringify({passedColumns: columns.[0].columns.map(c => c.id)})} */}
-      </pre>
-      <Table
-        columns={columns}
-        data={data}
-        enableColumnReordering={true}
-        emptyTableContent='No data.'
-      />
+      <Button>Hello world</Button>
     </>
   );
 };
